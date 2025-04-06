efdc44a7eeb169ccaa91ead939a72878039ba7dd
Viviana Kontos <kontoviv@students.zhaw.ch>
2025-04-06 15:00:59 +0200

    initialize spring backend

---
+++ backend/src/test/java/ch/zhaw/it/pm2/quizbot/QuizBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.it.pm2.quizbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

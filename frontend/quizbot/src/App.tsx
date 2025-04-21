d5dfc324d5e8d48f957a0c4d45556b78b11e7c57
SchViv <schoeviv@students.zhaw.ch>
2025-04-21 13:14:54 +0200

    added the beginning of the theme

--- frontend/quizbot/src/App.tsx
+++ frontend/quizbot/src/App.tsx
@@ -1,6 +1,9 @@
-import React, {useMemo, useState} from 'react';
+
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css';
+import {useMemo} from "react";
+const theme = useMemo(() => createTheme({}), []);
+
 
 function App() {
   return (
